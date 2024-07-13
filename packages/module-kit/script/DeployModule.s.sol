326af335543ce8512054a7cdc6b845b67175912f
Marcus Pang Yu Yang <marcuspangyuyang@gmail.com>
2024-07-13 18:20:22 +0200

    feat: update deploy script

--- packages/module-kit/script/DeployModule.s.sol
+++ packages/module-kit/script/DeployModule.s.sol
@@ -5,25 +5,27 @@ import "forge-std/Script.sol";
 import { RegistryDeployer } from "modulekit/deployment/RegistryDeployer.sol";
 
 // Import modules here
-import { ValidatorTemplate } from "src/ValidatorTemplate.sol";
+import { USDCSwapperExecute } from "src/USDCSwapperExecute.sol";
 
 /// @title DeployModuleScript
 contract DeployModuleScript is Script, RegistryDeployer {
+    address public usdc = 0x036CbD53842c5426634e7929541eC2318f3dCF7e;
+
     function run() public {
         // Setup module bytecode, deploy params, and data
-        bytes memory bytecode = type(ValidatorTemplate).creationCode;
+        bytes memory bytecode = type(USDCSwapperExecute).creationCode;
         bytes memory deployParams = "";
         bytes memory data = "";
 
         // Get private key for deployment
-        vm.startBroadcast(vm.envUint("PK"));
+        vm.startBroadcast();
 
         // Deploy module
         address module = deployModule({
             code: bytecode,
             deployParams: deployParams,
             salt: bytes32(0),
-            data: data
+            data: abi.encode(usdc, 0x1411060f44DA12017ADbB34De5f9238912dB2055)
         });
 
         // Stop broadcast and log module address
